099a09b27341cdfdbb08608a3273dcde0ba83625
Jamie Lamport <jamie.lamport@gmail.com>
2022-10-31 22:31:29 +0000

    Added further components and played with function vs class components

---
+++ src/components/TodosList.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import TodoItem from "./TodoItem.component";
+
+class TodosList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default TodosList;
